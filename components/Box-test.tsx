0632607e2328a88d82a1db1bd92c4b55ca8c971c
jerry <jerryaahe@gmail.com>
2026-04-07 18:35:28 +0800

    s

--- components/Box-test.tsx
+++ components/Box-test.tsx
@@ -1,3 +1,15 @@
+"use client"
+import { useTranslations } from "next-intl"
+import { debounce } from "lodash-es"
 export default function Box() {
-  return <div>box</div>
+  const t = useTranslations()
+  const handleSearch = debounce(() => {
+    console.log("Searching...")
+  }, 300)
+  return (
+    <div>
+      <button onClick={handleSearch}>search</button>
+      <div> {t("libs.title")}</div>
+    </div>
+  )
 }
